44c55029c4367bfc7bf39b2e172050775e1fd652
vishal Gupta <vishalgupta120703@gmail.com>
2025-04-26 10:46:45 +0530

    i am adding login.ejs

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const router = express.Router()
 router.get("/SignUp",(req,res)=>{
     res.render("auth/signUp.ejs")
 })
+router.get("/login",(req,res)=>{
+    res.render("auth/login.ejs")
+})
 
 
 module.exports = router
