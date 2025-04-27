dc2a56561d2890f945c9861c0c3155056f412ac7
vishal Gupta <vishalgupta120703@gmail.com>
2025-04-28 00:19:47 +0530

    adding post and get request in blog path

--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const router = express.Router()
+const blogController = require("../controller/blogController");
 
-router.get("/createBlog",(req,res)=>{
-    res.render("blog/createBlog.ejs")
-})
+router.get("/createBlog",blogController.getBlog)
+router.post("/createBlog",blogController.postBlog);
 
 module.exports= router;
