c8829dc87166987fc9f7c04c68343b9679118f2a
vishal Gupta <vishalgupta120703@gmail.com>
2025-05-11 12:47:12 +0530

    commit

--- controller/blogController.js
+++ controller/blogController.js
@@ -4,12 +4,28 @@ const multer = require('multer');
 exports.getBlog = (req,res)=>{
     res.render("blog/createBlog")
 }
-exports.postBlog = async(req,res)=>{
-    const {title,content,image,user,author,createdAt} = req.body;
-    const newBlog = Blog({title,content,image,user,author,createdAt});
+exports.postBlog = async (req, res) => {
+  try {
+    const { title, content, user, author, createdAt } = req.body;
+    const image = req.file ? req.file.filename : null;
+
+    const newBlog = new Blog({
+      title,
+      content,
+      image,
+      user,
+      author,
+      createdAt: createdAt || new Date()
+    });
+
     await newBlog.save();
     res.redirect("/blog/createBlog");
-}
+  } catch (error) {
+    console.error("Error posting blog:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
 exports.getAllBlog = async(req,res)=>{
     try{
         const blog = await Blog.find();
--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,23 @@
 const express = require("express")
 const router = express.Router()
 const blogController = require("../controller/blogController");
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+
 
 router.get("/createBlog",blogController.getBlog)
-router.post("/createBlog",blogController.postBlog);
+router.post("/createBlog",upload.single('image'),blogController.postBlog);
 router.get("/AllBlog",blogController.getAllBlog)
 
 module.exports= router;
