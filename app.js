5f2d3d6ea4a30fbf34c3eac9c7fbef726358dbdd
vishal Gupta <vishalgupta120703@gmail.com>
2025-05-12 17:22:47 +0530

    revert everthing file uploader

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cookieParser = require('cookie-parser');
 const path = require("path")
 main();
 app.use(express.static(path.join(__dirname,"public")))
+app.use('/uploads', express.static('upload'));
 app.set("view engine","ejs")
 app.set('views', path.join(__dirname, 'views'));
 app.use(cookieParser());
--- controller/blogController.js
+++ controller/blogController.js
@@ -1,29 +1,10 @@
 const Blog = require("../models/blogs/AllBlogs");
-const multer = require('multer');
 
 exports.getBlog = (req,res)=>{
     res.render("blog/createBlog")
 }
 exports.postBlog = async (req, res) => {
-  try {
-    const { title, content, user, author, createdAt } = req.body;
-    const image = req.file ? req.file.filename : null;
-
-    const newBlog = new Blog({
-      title,
-      content,
-      image,
-      user,
-      author,
-      createdAt: createdAt || new Date()
-    });
-
-    await newBlog.save();
-    res.redirect("/blog/createBlog");
-  } catch (error) {
-    console.error("Error posting blog:", error);
-    res.status(500).send("Internal Server Error");
-  }
+  res.send("file uploaded");
 };
 
 exports.getAllBlog = async(req,res)=>{
